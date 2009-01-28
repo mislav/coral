371a612ad86eee8ed11d40b6e38c21747bda4ec5
Mislav Marohnić <mislav.marohnic@gmail.com>
2009-01-28 02:49:40 +0100

    Coral now augments Kernel#require to automatically push available libraries to load path

--- lib/coral.rb
+++ lib/coral.rb
@@ -3,4 +3,23 @@ module Coral
   
   autoload :Runner, 'coral/runner'
   autoload :RemoteUrl, 'coral/remote_url'
+  
+  def self.repos
+    Dir.chdir(LocalReef) do
+      Dir["*"].to_a
+    end
+  end
+  
+  def self.find(repo_name)
+    repos.include?(repo_name) and Dir["#{LocalReef}/#{repo_name}/**/lib"].first
+  end
+  
+  def self.activate(coral_dir)
+    coral_dir += '/lib' unless coral_dir =~ %r{/lib$}
+    $LOAD_PATH.unshift coral_dir
+  end
+end
+
+unless 'coral' == File.basename($0)
+  require 'coral/custom_require'
 end
---
+++ lib/coral/custom_require.rb
@@ -0,0 +1,29 @@
+module Kernel
+  alias coral_original_require require
+
+  # Augment Ruby's require
+  #
+  def require(path)
+    if defined? ::Gem
+      gem_original_require(path)
+    else
+      coral_original_require(path)
+    end
+  rescue LoadError => load_error
+    if load_error.message =~ /\A[Nn]o such file to load -- #{Regexp.escape path}\z/ and
+        lib_dir = Coral.find(path)
+      Coral.activate(lib_dir)
+      # library added to load paths; now retry require
+      if defined? ::Gem
+        gem_original_require(path)
+      else
+        coral_original_require(path)
+      end
+    elsif defined? ::Gem
+      # try load with RubyGems
+      coral_original_require(path)
+    else
+      raise load_error
+    end
+  end
+end
