8bda2f81fac3f1386e8293d9c1468946752473b5
Mislav Marohnić <mislav.marohnic@gmail.com>
2009-02-26 15:29:20 +0100

    fix overloading of `require` so that it doesn't fail when RubyGems is loaded after Coral

--- lib/coral/custom_require.rb
+++ lib/coral/custom_require.rb
@@ -1,26 +1,18 @@
 module Kernel
-  alias coral_original_require require
+  if defined? ::Gem
+    alias coral_original_require gem_original_require
+  else                          
+    alias coral_original_require require
+  end
 
-  # Augment Ruby's require
-  #
+  ## Augment ruby's require
   def require(path)
-    if defined? ::Gem
-      gem_original_require(path)
-    else
-      coral_original_require(path)
-    end
+    coral_original_require(path)
   rescue LoadError => load_error
     if load_error.message =~ /\A[Nn]o such file to load -- #{Regexp.escape path}\z/ and
         coral_dir = Coral.find(path)
       Coral.activate(coral_dir)
       # library added to load paths; now retry require
-      if defined? ::Gem
-        gem_original_require(path)
-      else
-        coral_original_require(path)
-      end
-    elsif defined? ::Gem
-      # try load with RubyGems
       coral_original_require(path)
     else
       raise load_error
