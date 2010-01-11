5dab528d467c4d0116e40ee7649571bad907ec00
Mislav Marohnić <mislav.marohnic@gmail.com>
2010-01-11 11:53:03 +0100

    when overloading `Kernel#require`, don't detect and special-case `gem_original_require`
    if you want coral loading have precedence over gem loading,
    ensure that Coral is loaded before RubyGems

--- lib/coral/custom_require.rb
+++ lib/coral/custom_require.rb
@@ -1,21 +1,19 @@
 module Kernel
-  if defined? ::Gem
-    alias coral_original_require gem_original_require
-  else                          
-    alias coral_original_require require
-  end
-
-  ## Augment ruby's require
+  alias coral_original_require require
+  
+  # augment ruby's require
   def require(path)
     coral_original_require(path)
-  rescue LoadError => load_error
-    if load_error.message =~ /\A[Nn]o such file to load -- #{Regexp.escape path}\z/ and
-        coral_dir = Coral.find(path)
-      Coral.activate(coral_dir)
+  rescue LoadError
+    if $!.message =~ /#{Regexp.escape path}\z/ and repo = Coral.find(path)
+      Coral.activate(repo)
       # library added to load paths; now retry require
       coral_original_require(path)
     else
-      raise load_error
+      raise
     end
   end
+  
+  private :require
+  private :coral_original_require
 end
